3b4bd751b06d273d62fb0b648171daa1bacfc9b1
anhnamitus <nam.nguyen-anh@banvien.com.vn>
2022-08-15 22:15:19 +0700

    [ADD] header to app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hi there!!</h1>
+    <BrowserRouter>
+      <Header />
       <MarketingApp />;
-    </div>
+    </BrowserRouter>
   );
 }
